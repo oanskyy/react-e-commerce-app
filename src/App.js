f5a25b65c845f597837911857f28b7862441b488
oanskyy <oana.anghelachi@gmail.com>
2023-11-30 18:59:23 +0200

    feat: set up base structure for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-
-
 const App = () => {
-	return <div>Hello beautiful world</div>
+	return (
+		<div className='categories-container'>
+			<div className='category-container'>
+				{/*	<img /> */}
+				<div className='category-body-container'></div>
+
+				<h2>Hats</h2>
+				<p>Shop Now</p>
+			</div>
+			<div className='category-container'>
+				{/*	<img /> */}
+				<div className='category-body-container'></div>
+
+				<h2>Jackets</h2>
+				<p>Shop Now</p>
+			</div>
+			<div className='category-container'>
+				{/*	<img /> */}
+				<div className='category-body-container'></div>
+
+				<h2>Sneakers</h2>
+				<p>Shop Now</p>
+			</div>
+			<div className='category-container'>
+				{/*	<img /> */}
+				<div className='category-body-container'></div>
+
+				<h2>Womens</h2>
+				<p>Shop Now</p>
+			</div>
+			<div className='category-container'>
+				{/*	<img /> */}
+				<div className='category-body-container'></div>
+
+				<h2>Mens</h2>
+				<p>Shop Now</p>
+			</div>
+		</div>
+	)
 }
 
 export default App
