88fc4aa46d21b0b4d02af8e4f697f7653a5b5c00
oanskyy <oana.anghelachi@gmail.com>
2023-12-06 22:35:36 +0200

    feat: create sign-up, sign-in component, form component, button component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react"
 import { Outlet, Link } from "react-router-dom"
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+
 import "./navigation.styles.scss"
 
 const Navigation = () => {
@@ -11,12 +12,11 @@ const Navigation = () => {
 				<Link className='logo-container' to='/'>
 					<CrwnLogo className='logo' />
 				</Link>
-
 				<div className='nav-links-container'>
 					<Link className='nav-link' to='/shop'>
 						SHOP
 					</Link>
-					<Link className='nav-link' to='/sign-in'>
+					<Link className='nav-link' to='/auth'>
 						SIGN IN
 					</Link>
 				</div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,50 +1,75 @@
 import { initializeApp } from "firebase/app"
-import { getAuth, signInWithPopup,signInWithRedirect, GoogleAuthProvider } from "firebase/auth"
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword
+} from "firebase/auth"
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCmDnI2YF_mKe7VbMnvvSFQUpwtegaAI04",
-	authDomain: "crwn-clothing-db-77312.firebaseapp.com",
-	projectId: "crwn-clothing-db-77312",
-	storageBucket: "crwn-clothing-db-77312.appspot.com",
-	messagingSenderId: "1073669151355",
-	appId: "1:1073669151355:web:44b5fa9c9f476078a960de"
+	apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
+	authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
+	projectId: "crwn-clothing-db-98d4d",
+	storageBucket: "crwn-clothing-db-98d4d.appspot.com",
+	messagingSenderId: "626766232035",
+	appId: "1:626766232035:web:506621582dab103a4d08d6"
 }
 
-// Initialize Firebase
-initializeApp(firebaseConfig)
-// const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig)
 
-// initialize provider
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider()
+
+googleProvider.setCustomParameters({
 	prompt: "select_account"
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () =>
+	signInWithRedirect(auth, googleProvider)
 
-// instantiate
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async userAuth => {
+export const createUserDocumentFromAuth = async (
+	userAuth,
+	additionalInformation = {}
+) => {
+	if (!userAuth) return
+
 	const userDocRef = doc(db, "users", userAuth.uid)
-	console.log(userDocRef)
 
 	const userSnapshot = await getDoc(userDocRef)
-	console.log(userSnapshot)
-	console.log(userSnapshot.exists())
 
 	if (!userSnapshot.exists()) {
 		const { displayName, email } = userAuth
 		const createdAt = new Date()
+
 		try {
-			await setDoc(userDocRef, { displayName, email, createdAt })
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+				...additionalInformation
+			})
 		} catch (error) {
 			console.log("error creating the user", error.message)
 		}
 	}
+
 	return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return
+
+	return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return
+
+	return await signInWithEmailAndPassword(auth, email, password)
 }
