cdab5ab7fc1e6a5ac2b739b140fd76994811f8c0
oanskyy <oana.anghelachi@gmail.com>
2023-12-17 23:31:16 +0200

    feat: addCollectionAndDOcuments

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,18 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js"
 
-import PRODUCTS from "../shop-data.json"
+// import PRODUCTS from "../shop-data.json"
+// import SHOP_DATA from "../shop-data.js"
 
 export const ProductsContext = createContext({
 	products: []
 })
 
 export const ProductsProvider = ({ children }) => {
-	const [products] = useState(PRODUCTS)
+	const [products, setProducts] = useState([])
+	// useEffect(()=>{
+	// 	addCollectionAndDocuments('categories', SHOP_DATA)
+	// }, [])
 	const value = { products }
 
 	return (
