9fc51dd197d120194ea08cb2fd72e6f8ae6d4a2a
oanskyy <oana.anghelachi@gmail.com>
2023-12-20 20:21:57 +0200

    fix: fix clashing styles

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import ProductCard from "../../components/product-card/product-card.component"
 
 import { CategoriesContext } from "../../contexts/categories.context"
+import './category.styles.scss'
 
 const Category = () => {
 	const { category } = useParams()
